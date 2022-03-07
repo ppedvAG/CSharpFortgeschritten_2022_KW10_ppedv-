1a179fa09b4eb0244b3dc6b280d164bf9fdfd1c2
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-03-07 10:02:22 +0100

    Agenda.md SOLID_SingleResponsibilityPrincipe.csproj

---
+++ Agenda.md
@@ -0,0 +1,43 @@
+PPEDV-Kurs:07.03.2022 -> C# Fortgeschritten
+Trainer: Kevin Winter
+Email: KevinW@ppedv.de
+
+Themen
+
+Montag 
+
+- [ ] Solid 
+- [ ] Delegates / Actions / Func / Events / EventHandler
+- [ ] Benchmark StringBuilder vs +-Operator (bei strings)
+- [ ] Interfaces vs abstract in Verbindung mit dem Builder Design Pattern 
+
+Dienstag
+- [ ] Threading
+- [ ] TPL
+- [ ] async / await
+- [ ] Genrics
+
+Mittwoch:
+
+- [ ] C# 7.0 -> C# 10.0 Sprach Features
+		-> https://docs.microsoft.com/de-de/dotnet/csharp/programming-guide/classes-and-structs/access-modifiers
+- [ ] Reflections 
+ - [ ] Plugin Pattern (Optional, CodeReview)
+- [ ] Serialisierung
+- [ ] Linq / Lambda 
+- [ ] Erweiterungsmethoden  
+- [ ] Operatoren
+- [ ] Anonyme Typen + dynamische Typen
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CSharpAdvancedKurs/SOLID_SingleResponsibilityPrincipe/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SOLID_SingleResponsibilityPrincipe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    #region BadCode
+
+    public class BadEmployee
+    {
+        //Datenstruktur
+        public int Id { get; set; } 
+        public string Name { get; set; }
+
+
+        //Methode im DataAccess Layer
+        public void InsertEmployeeToTable(BadEmployee employee)
+        {
+            //any Code
+        }
+
+
+        //Export-Methode - Service Layer oder Export->UI Präsentation-Layer
+        public void GenerateReport()
+        {
+            //any Code
+        }
+    }
+    #endregion
+
+    #region Bessere Variante
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+    }
+
+    public class EmployeeRepository
+    {
+        public void Insert(Employee employee)
+        {
+            //Any Code
+        }
+    }
+
+    public class GenerateReport
+    {
+        public void Generate()
+        {
+            //any Code
+        }
+    }
+    #endregion
+}
