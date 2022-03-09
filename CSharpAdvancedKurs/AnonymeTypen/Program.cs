baad350b861ef730fcac04435e6337f732ce3225
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-03-09 15:36:49 +0100

    AnonymeTypen

---
+++ CSharpAdvancedKurs/AnonymeTypen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Dynamic;
+
+dynamic myObj = new ExpandoObject();
+
+myObj.Id = 123;
+myObj.Name = "Gustav Gans";
+myObj.Title = "Glückspilz";
+myObj.Feierabend = true;
+
+
+if (myObj.Feierabend)
+{
+    Console.WriteLine("Feierabend!");
+}
